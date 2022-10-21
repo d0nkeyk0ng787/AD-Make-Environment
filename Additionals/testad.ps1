0e63cc1c2842c5d79aeb1a468af6be31d98b7d27
Neb <77905135+d0nkeyk0ng787@users.noreply.github.com>
2022-10-21 22:38:47 +1100

    Update testad.ps1

--- Additionals/testad.ps1
+++ Additionals/testad.ps1
@@ -1,13 +1,39 @@
 # Test if AD has finished on the DC and it is ready to join the client systems
 
+$chars = "/", "-", "\", "|"
+$state = 1
+$counter = 0
+$mult = 1
+$invokecounter = 0
+
 function Get-ADReady{
     param(
     [parameter(Mandatory=$true)][string[]]$VMName,
     [parameter(Mandatory=$true)]$Creds
     )
-
-    Write-Host "Trying to retrieve the AD Object!..." -ForegroundColor Black -BackgroundColor Magenta
-    while((Invoke-Command -VMName $DC1 -Credential $Creds -ScriptBlock{Get-ADObject -Filter * -ErrorAction SilentlyContinue}) -eq $null){Start-Sleep 5}
-    Write-Host "AD object retrieved...Continuing!" -ForegroundColor Black -BackgroundColor Green
-
+    Write-Host "Trying to retrieve the AD Object!..." -ForegroundColor Green -BackgroundColor Black
+    for ($i=0;$i -le 5; $i++) {
+        if($script:invokecounter -eq 60){
+            if((Invoke-Command -VMName $VMName -Credential $Creds -ScriptBlock{Get-ADObject -Filter * -ErrorAction SilentlyContinue}) -eq $null){
+                continue
+            }
+            else{
+                Write-Host "`r`nAD object retrieved...Continuing!" -ForegroundColor Black -BackgroundColor Magenta
+                break
+            }
+        }
+        if($i -eq 4){
+            $i = 0
+        }
+        if($script:counter -eq 20){
+            $script:counter = 0
+            $script:mult++
+        }
+        $progress = $chars[$i] * $mult
+        Write-Host -NoNewLine "`r$progress" -ForegroundColor Green -BackgroundColor Black
+        #Write-Host -NoNewline $script:invokecounter
+        $script:counter++
+        $script:invokecounter++
+        Start-Sleep -Milliseconds 150
+    }
 }
